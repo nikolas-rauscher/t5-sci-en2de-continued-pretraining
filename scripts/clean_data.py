11dd6aa8d260412c9375f05eaa10fe3896ddba37
Nikolas Rauscher <nikolas.rauscher@gmail.com>
2025-05-19 14:25:40 +0200

    Add clean_data.py script for the first data preprocessing pipeline
     sets up a data cleaning pipeline using DataTrove components, including reading from Parquet files, filtering, and writing output.
    
    Signed-off-by: Nikolas Rauscher <nikolas.rauscher@gmail.com>

---
+++ scripts/clean_data.py
@@ -0,0 +1,39 @@
+import hydra
+from omegaconf import DictConfig, OmegaConf
+from datatrove.executor import LocalPipelineExecutor
+from datatrove.pipeline.readers import ParquetReader
+from datatrove.pipeline.writers import ParquetWriter
+from datatrove.pipeline.filters import URLFilter, SamplerFilter
+from datatrove.pipeline.stats import DocStats
+
+import os, sys
+# Ensure project root is on PYTHONPATH  for src package
+script_dir = os.path.dirname(__file__)
+proj_root = os.path.abspath(os.path.join(script_dir, '..'))
+sys.path.insert(0, proj_root)
+
+from src.dataprep.cleaning_blocks import StructMetaToJSON
+
+@hydra.main(config_path="../configs", config_name="cleaning/datatrove")
+def main(cfg: DictConfig) -> None:
+
+
+    pipeline = [
+        ParquetReader(data_folder=cfg.cleaning.paths.src_dir, glob_pattern=cfg.cleaning.paths.src_pattern, limit=cfg.cleaning.limit_documents),
+        #URLFilter(),
+        #StructMetaToJSON(),
+        SamplerFilter(),
+        DocStats(output_folder=cfg.cleaning.paths.dst + "/stats", groups_to_compute=["summary"]),
+        ParquetWriter(cfg.cleaning.paths.dst),
+    ]
+
+    executor = LocalPipelineExecutor(
+        pipeline=pipeline,
+        tasks=cfg.cleaning.cleaning.tasks,
+        workers=cfg.cleaning.cleaning.workers,
+    )
+    executor.run()
+
+
+if __name__ == "__main__":
+    main()
